d7a8ee46528b107ac5f0c2d1e169b13cf02da13c
DmitryBul <dmitrybulak4@gmail.com>
2026-03-27 19:46:15 +0100

    Add UserService

---
+++ src/main/java/com/example/demo/authservice/service/UserService.java
@@ -0,0 +1,21 @@
+package com.example.demo.authservice.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.authservice.model.User;
+import com.example.demo.authservice.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public User register(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+}
