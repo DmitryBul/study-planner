2d587f5ad92629a8cd92e6b1e2bdd5a818129796
DmitryBul <dmitrybulak4@gmail.com>
2026-03-28 19:22:56 +0100

    Add function to find user by email

--- src/main/java/com/example/demo/authservice/service/UserService.java
+++ src/main/java/com/example/demo/authservice/service/UserService.java
@@ -18,4 +18,9 @@ public class UserService {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
+
+    public User findByEmail(String email) {
+    return userRepository.findByEmail(email)
+            .orElseThrow(() -> new RuntimeException("User not found"));
+}
 }
