ffd21ceb8990af1d8428eb44ffc8ace6d7c8ab08
DmitryBul <dmitrybulak4@gmail.com>
2026-05-11 21:22:18 +0200

    Create TaskController

---
+++ src/main/java/com/example/demo/task/controller/TaskController.java
@@ -0,0 +1,38 @@
+package com.example.demo.task.controller;
+
+import com.example.demo.task.model.Task;
+import com.example.demo.task.service.TaskService;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/tasks")
+@RequiredArgsConstructor
+public class TaskController {
+
+    private final TaskService taskService;
+
+    @PostMapping
+    public Task createTask(@RequestBody Task task) {
+        return taskService.createTask(task);
+    }
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return taskService.getAllTasks();
+    }
+
+    @GetMapping("/{id}")
+    public Task getTaskById(@PathVariable Long id) {
+        return taskService.getTaskById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteTask(@PathVariable Long id) {
+        taskService.deleteTask(id);
+        return "Task deleted";
+    }
+}
